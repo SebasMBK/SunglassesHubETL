ed544de2561646a734321037e431b24e25724819
SebasMBK <sebastianmbk@hotmail.com>
2022-11-28 16:41:26 -0500

    init

--- az_login.sh
+++ az_login.sh
@@ -2,8 +2,11 @@
 echo "Please wait for the authentication code to appear"
 if docker exec -ti -u airflow sunglasseshubetl_airflow-worker_1 az login ; then
     echo "Logged in successfully"
+elif docker exec -ti -u airflow sunglasseshubetl-airflow-worker-1 az login ; then
+    echo "Logged in successfully"
 else
-    docker exec -ti -u airflow sunglasseshubetl-airflow-worker-1 az login
+    echo "Seems like an error ocurred. Please use this command:"
+    echo "docker exec -ti -u airflow [name_of_the_airflow_worker_container] az login"
 fi
 
 #If this script doesn't work use this command:
